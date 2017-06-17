9a84c0d060c192045cf13b7c0fc9fd84249c85a9
Quentin Bazin <quent42340@gmail.com>
2017-06-17 21:59:19 +0200

    [ArcherFactory] Speed increased.

--- source/scene/factories/ArcherFactory.cpp
+++ source/scene/factories/ArcherFactory.cpp
@@ -46,7 +46,7 @@ SceneObject ArcherFactory::create() {
 	animationListComponent.setAnimationEndCallback(std::bind(&ArcherShootBehaviour::animationEndCallback, &archerShootBehaviour, std::placeholders::_1, std::placeholders::_2));
 	for (const char *animName : animNames) {
 		Animation &anim = animationListComponent.add(bodyEntity, animName);
-		anim.speed = (anim.name != "Attack") ? 1.75f : 1.0f;
+		anim.speed = (anim.name != "Attack") ? 1.75f : 4.0f;
 	}
 
 	animationListComponent.setLoop("Attack", false);
